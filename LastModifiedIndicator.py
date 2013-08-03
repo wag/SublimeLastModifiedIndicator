554c098399ff8da6dad405b8c0d59e0ddc5e73ee
Jonas Wagner <wagnercode@gmail.com>
2013-08-03 15:22:07 +0200

    ST3 support, settings reloader

--- LastModifiedIndicator.py
+++ LastModifiedIndicator.py
@@ -2,9 +2,52 @@ import sublime_plugin
 import sublime
 import os
 
-BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-IMG_PATH = os.path.join('..', os.path.basename(BASE_PATH), 'img')
-settings = sublime.load_settings('LastModifiedIndicator.sublime-settings')
+BASE_PATH = os.path.basename(os.path.abspath(os.path.dirname(__file__)))
+if int(sublime.version()) < 3014:
+    PATH = '..'
+    EXTN = ''
+else:
+    PATH = 'Packages'
+    EXTN = '.png'
+IMG_PATH = os.path.join(PATH, BASE_PATH, 'img').replace('\\', '/')
+
+ALL_SETTINGS = [
+    'last_modified_indicator',
+    'last_modified_indicator_multiline',
+]
+
+
+def settings_changed():
+    for window in sublime.windows():
+        for view in window.views():
+            reload_settings(view)
+            erase_regions(view)
+
+
+def reload_settings(view):
+    settings = sublime.load_settings('LastModifiedIndicator.sublime-settings')
+    settings.clear_on_change(__name__)
+    settings.add_on_change(__name__, settings_changed)
+
+    view_settings = view.settings()
+    for setting in ALL_SETTINGS:
+        if settings.get(setting) is not None:
+            view_settings.set(setting, settings.get(setting))
+
+    if view_settings.get('last_modified_indicator') is None:
+        view_settings.set('last_modified_indicator', True)
+
+    return view_settings
+
+
+def plugin_loaded():
+    settings_changed()
+
+
+def erase_regions(view):
+    for i in range(-3, 4):
+        view.erase_regions('lmi-outline-{0}'.format(i))
+
 
 class LastModifiedIndicator(object):
     def __init__(self, view):
@@ -14,28 +57,34 @@ class LastModifiedIndicator(object):
 
     @property
     def _range(self):
-        return range(-3, 4) if settings.get('last_modified_indicator_multiline', True) else range(0, 1)
+        return range(-3, 4) if self.view.settings().get('last_modified_indicator_multiline', True) else range(0, 1)
 
     def run(self):
         if self.has_sel:
-            self.erase_regions()
             line = self.view.rowcol(self.view.sel()[0].begin())[0]
+            erase_regions(self.view)
             for i in self._range:
                 _line = line + i
                 if _line < 0:
                     continue
                 point = self.view.full_line(self.view.text_point(_line, 0))
-                if os.path.exists(os.path.join(BASE_PATH, 'img', '%d%s' % (abs(i), '.png'))):
-                    self.view.add_regions('lmi-outline-%d' % i, [point, ], 'lmi.outline.%d' % i,
-                        os.path.join(IMG_PATH, str(abs(i))), sublime.HIDDEN)
-
-    def erase_regions(self):
-        if self.has_sel:
-            for i in range(-3, 4):
-                self.view.erase_regions('lmi-outline-%d' % i)
+                self.view.add_regions(
+                    'lmi-outline-{0}'.format(i), [point, ],
+                    'lmi.outline.{0}'.format(i),
+                    os.path.join(IMG_PATH, format(abs(i)) + EXTN),
+                    sublime.HIDDEN)
 
 
 class LastModifiedIndicatorEventHandler(sublime_plugin.EventListener):
+    def on_load(self, view):
+        reload_settings(view)
+
+    def on_new(self, view):
+        reload_settings(view)
+
+    def on_clone(self, view):
+        reload_settings(view)
+
     def on_modified(self, view):
-        if settings.get('last_modified_indicator', True):
+        if view.settings().get('last_modified_indicator', True):
             LastModifiedIndicator(view).run()
